6715b5a4bee771530dbf199a8c2ae1af91e3757a
kassama fally <fally.kassama@outlook.com>
2019-11-20 11:50:46 +0100

    first commit

---
+++ Casino.py
@@ -0,0 +1,12 @@
+import os
+import random
+
+Continue = True
+Bourse =1000
+
+while Continue:
+        mise = 0
+     while mise <0 or mise > 49:
+        mise= input("choisi entre 0 et 49 le numero ou tu mise")
+        mise=int(mise)
+    
