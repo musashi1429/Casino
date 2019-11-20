c9e71a7285d4874b8b24bc4f7baf849cc960b0b9
kassama fally <fally.kassama@outlook.com>
2019-11-20 12:02:15 +0100

    add mise and function

--- Casino.py
+++ Casino.py
@@ -5,8 +5,17 @@ Continue = True
 Bourse =1000
 
 while Continue:
-        mise = 0
-     while mise <0 or mise > 49:
-        mise= input("choisi entre 0 et 49 le numero ou tu mise")
-        mise=int(mise)
-    
+    numero_mise = 0
+    while mise <0 or mise > 49:
+        numero_mise= input("choisi entre 0 et 49 le numero ou tu mise")
+        numero_mise=int(numero_mise)
+        if numero_mise < 0 or numero_mise > 49:
+            print("ce nombre n'est pas compris entre 0 et 49")
+    mise = 0
+    while mise <= 0 or mise > Bourse:
+        mise = input("mettez le montant à parié")
+        mise = int(mise)
+
+        if mise <= 0 or mise > Bourse:
+            print("vous ne pouvez pas miser plus que vous avez et la maison ne fait pas credit !!!")
+        
